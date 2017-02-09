6c69425bce91add7dc5fb139ef850c4453cf54b5
Sorin.C <sorin.chiprian@users.noreply.github.com>
2017-02-09 11:04:07 +0200

    add utf8 encode/decode

---
+++ src/main/scala/monix/io/text/UTF8Codec.scala
@@ -0,0 +1,146 @@
+package monix.io.text
+
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+
+import monix.execution.Ack.{Continue, Stop}
+import monix.execution.atomic.Atomic
+import monix.execution.{Ack, Cancelable}
+import monix.reactive.exceptions.MultipleSubscribersException
+import monix.reactive.observers.Subscriber
+import monix.reactive.subjects.Subject
+import monix.reactive.{Observable, Observer, Pipe}
+
+import scala.concurrent.Future
+
+object UTF8Codec {
+  case object utf8Decode extends Pipe[Array[Byte], String]{
+    override def unicast: (Observer[Array[Byte]], Observable[String]) = {
+      val p = new UTF8DecodingSubject()
+      (p,p)
+    }
+  }
+
+  case object utf8Encode extends Pipe[String, Array[Byte]] {
+    override def unicast: (Observer[String], Observable[Array[Byte]]) = {
+      val p = new UTF8EncodingSubject()
+      (p,p)
+    }
+  }
+  private val utf8Charset = Charset.forName("UTF-8")
+
+  private[text] class UTF8DecodingSubject extends Subject[Array[Byte], String] {
+    private[this] val subscriber = Atomic(Option.empty[Subscriber[String]])
+    private[this] val stopOnNext = Atomic(false)
+
+    val remaining = ByteBuffer.allocate(4)
+
+    override def size: Int =
+      if (subscriber.get.nonEmpty) 1 else 0
+
+    override def unsafeSubscribeFn(subscriber: Subscriber[String]): Cancelable = {
+      if (!this.subscriber.compareAndSet(None, Some(subscriber))) {
+        subscriber.onError(MultipleSubscribersException(this.getClass.getName))
+        Cancelable.empty
+      } else {
+        remaining.put(0,0)
+        Cancelable(() => stopOnNext.set(true))
+      }
+    }
+
+    override def onError(ex: Throwable): Unit = subscriber.get.foreach(_.onError(ex))
+    override def onComplete(): Unit = subscriber.get.foreach(_.onComplete())
+
+    override def onNext(elem: Array[Byte]): Future[Ack] = {
+      if (stopOnNext.get || subscriber.get.isEmpty) {
+        //we stop if no subscriber or canceled from outside
+        Stop
+      } else {
+        val remainingArray = remaining.array()
+        val oldRemaining =
+          if (remainingArray(0) == 0) Array.empty[Byte]
+          else remainingArray.slice(1, 1+remainingArray(0))
+        val (current, newRemaining) = getString(elem, oldRemaining)
+        remaining.clear()
+        remaining.put(newRemaining.length.toByte)
+        remaining.put(newRemaining)
+        current match {
+          case Some(str) => subscriber.get.get.onNext(str)
+          case _ => Continue
+        }
+      }
+    }
+
+    private def getString(elem: Array[Byte], oldBytes: Array[Byte]): (Option[String], Array[Byte]) = {
+      val bytes = oldBytes ++ elem
+      val splitAt = getSplitAt(bytes)
+
+      splitAt match {
+        case Some(split) if split == 0 =>
+          val newRemaining = bytes
+          (None, newRemaining)
+
+        case Some(split) if split > 0 =>
+
+          val str = new String(bytes.take(split), utf8Charset)
+          val newRemaining = bytes.drop(split)
+          (Some(str), newRemaining)
+
+        case _ =>
+          val str = new String(bytes, utf8Charset)
+          (Some(str), Array.empty[Byte])
+      }
+    }
+
+    private def indexIncrement(b: Byte): Int  = {
+      if ((b & 0x80) == 0) 0 // ASCII byte
+      else if ((b & 0xE0) == 0xC0) 2 // first of a 2 byte seq
+      else if ((b & 0xF0) == 0xE0) 3 // first of a 3 byte seq
+      else if ((b & 0xF8) == 0xF0) 4 // first of a 4 byte seq
+      else 0 // following char
+    }
+
+    private def getSplitAt(bytes: Array[Byte]) = {
+      val lastThree = bytes.drop(0 max bytes.length - 3)
+      val addBytesFromLast3 = lastThree.zipWithIndex.foldLeft(Option.empty[Int]){ (acc, elem) =>
+        val increment = indexIncrement(elem._1)
+        val index = elem._2
+        if (index + increment > lastThree.length) {
+          Some(index)
+        } else {
+          acc
+        }
+      }
+      addBytesFromLast3 map (bytes.length + _ - lastThree.length)
+    }
+  }
+
+  private[text] class UTF8EncodingSubject extends Subject[String, Array[Byte]] {
+    private[this] val subscriber = Atomic(Option.empty[Subscriber[Array[Byte]]])
+    private[this] val stopOnNext = Atomic(false)
+
+    override def size: Int = if (subscriber.get.nonEmpty) 1 else 0
+
+    override def unsafeSubscribeFn(subscriber: Subscriber[Array[Byte]]): Cancelable = {
+      if (!this.subscriber.compareAndSet(None, Some(subscriber))) {
+        subscriber.onError(MultipleSubscribersException(this.getClass.getName))
+        Cancelable.empty
+      } else {
+        Cancelable(() => stopOnNext.set(true))
+      }
+    }
+
+    override def onError(ex: Throwable): Unit = subscriber.get.foreach(_.onError(ex))
+    override def onComplete(): Unit = subscriber.get.foreach(_.onComplete())
+
+    override def onNext(elem: String): Future[Ack] = {
+      if (stopOnNext.get || subscriber.get.isEmpty) {
+        //we stop if no subscriber or canceled from outside
+        Stop
+      }
+      else {
+        subscriber.get.get.onNext(elem.getBytes(utf8Charset))
+      }
+    }
+  }
+}
--- src/test/scala/monix/io/Test.scala
+++ src/test/scala/monix/io/Test.scala
@@ -2,10 +2,13 @@ package monix.io
 
 import java.nio.file.{Files, Paths}
 import java.util
+
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.FunSuite
 import file._
 import monix.eval.Callback
+import monix.io.text.UTF8Codec._
+import monix.reactive.Observable
 
 import scala.concurrent.{Await, Promise}
 import scala.concurrent.duration._
@@ -16,7 +19,7 @@ class Test extends FunSuite with LazyLogging{
   test("same file generated") {
     implicit val ctx = monix.execution.Scheduler.Implicits.global
 
-    val from = Paths.get(this.getClass.getResource("/testFiles/file.txt").toURI())
+    val from = Paths.get(this.getClass.getResource("/testFiles/file.txt").toURI)
     val to = Paths.get("src/test/resources/out.txt")
     val consumer = new AsyncFileWriterConsumer(to)
     val p = Promise[Boolean]()
@@ -25,7 +28,9 @@ class Test extends FunSuite with LazyLogging{
       override def onError(ex: Throwable): Unit = p.failure(ex)
     }
 
-    readAsync(from, 3).consumeWith(consumer).runAsync(callback)
+    readAsync(from, 3)
+      .consumeWith(consumer)
+      .runAsync(callback)
     val result = Await.result(p.future, 3.second)
     assert(result, true)
 
@@ -35,6 +40,53 @@ class Test extends FunSuite with LazyLogging{
 
     //clean
     Files.delete(to)
+  }
+
+  test("decode file utf8") {
+    implicit val ctx = monix.execution.Scheduler.Implicits.global
+
+    val from = Paths.get(this.getClass.getResource("/testFiles/specialChars.txt").toURI)
+
+    val p = Promise[Seq[Byte]]()
+    val callback = new Callback[List[Array[Byte]]] {
+      override def onSuccess(value: List[Array[Byte]]): Unit = p.success(value.flatten)
+      override def onError(ex: Throwable): Unit = p.failure(ex)
+    }
+
+    readAsync(from, 3)
+      .pipeThrough(utf8Decode)
+      .pipeThrough(utf8Encode)
+      .toListL
+      .runAsync(callback)
+    val result = Await.result(p.future, 3.second)
+    val f1 = Files.readAllBytes(from)
+    val f2 = result
+    assert(util.Arrays.equals(f1, f2.toArray))
+  }
+
+  test("decode special chars") {
+    implicit val ctx = monix.execution.Scheduler.Implicits.global
+    val strSeq = Seq("A", "\u0024", "\u00A2", "\u20AC", new String(Array(0xF0, 0x90, 0x8D, 0x88).map(_.toByte)), "B")
+
+    for (grouping <- 1 to 12) {
+      val obsSeq =
+        Observable
+          .fromIterator(strSeq.flatMap(_.getBytes).grouped(grouping).map(_.toArray))
+          .pipeThrough(utf8Decode)
+
+      val p = Promise[Boolean]()
+      val callback = new Callback[List[String]] {
+        override def onSuccess(value: List[String]): Unit = {
+          p.success(if (value.mkString == strSeq.mkString) true else false)
+        }
+
+        override def onError(ex: Throwable): Unit = p.failure(ex)
+      }
+      obsSeq.toListL.runAsync(callback)
+      val result = Await.result(p.future, 3.second)
+      if (!result) info(s"grouping=$grouping")
+      assert(result)
+    }
   }
 
 
